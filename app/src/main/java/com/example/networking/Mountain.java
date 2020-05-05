e318c1d4ae54130206e49a404e3286645c659285
RindiRio95 <a19anjst@student.his.se>
2020-05-05 13:59:44 +0200

    La till constructor

--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -1,4 +1,19 @@
 package com.example.networking;
 
 public class Mountain {
+    private String name;
+    private String location;
+    private int height;
+
+    public Mountain(String inName,String inLocation, int inHeight){
+        name=inName;
+        location=inLocation;
+        height=inHeight;
+    }
+
+    public Mountain(String inName){
+        name=inName;
+        location="";
+        height=1;
+    }
 }
